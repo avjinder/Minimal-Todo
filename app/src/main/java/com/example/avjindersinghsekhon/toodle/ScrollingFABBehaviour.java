05dece2bffb7db3bbe350e567b486296c39ab3fc
avjindersinghsekhon <avisekhon@gmail.com>
2015-09-08 03:03:32 +0530

    New FAB Behaviour class that depends on Toolbar height to move offscreen.
    FAB now scrolls offscreen when list is scrolled.

---
+++ app/src/main/java/com/example/avjindersinghsekhon/toodle/ScrollingFABBehaviour.java
@@ -0,0 +1,33 @@
+package com.example.avjindersinghsekhon.toodle;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ScrollingFABBehaviour extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    private int toolbarHeight;
+    public  ScrollingFABBehaviour(Context context, AttributeSet attributeSet){
+        super(context, attributeSet);
+        this.toolbarHeight = Utils.getToolbarHeight(context);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        if(dependency instanceof AppBarLayout){
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams)child.getLayoutParams();
+            int fabBottomMargin = lp.bottomMargin;
+            int distanceToScroll = child.getHeight() + fabBottomMargin;
+            float ratio = dependency.getY() /(float)toolbarHeight;
+            child.setTranslationY(-distanceToScroll*ratio);
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/avjindersinghsekhon/toodle/Utils.java
@@ -0,0 +1,18 @@
+package com.example.avjindersinghsekhon.toodle;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+
+public class Utils {
+
+
+        public static int getToolbarHeight(Context context) {
+            final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                    new int[]{R.attr.actionBarSize});
+            int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+            styledAttributes.recycle();
+
+            return toolbarHeight;
+        }
+}
