5ef8545b5bf51d7442ce24aadf55ee1b83e715ea
Bala <balavignesh_b@live.com>
2019-07-04 15:29:52 -0400

    Bala Commit test case

---
+++ app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/Main/Bala/BalaTestSuite.java
@@ -0,0 +1,15 @@
+package com.example.avjindersinghsekhon.minimaltodo.Main.Bala;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Runs all unit tests.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({DeletePageTest.class ,
+
+        ListPageTest.class,EditClosePageTest.class
+,EditModifyClosePageTest.class,EditModifySavePageTest.class,EditVerifyClosePageTest.class,DeleteUndoPageTest.class,})
+public class BalaTestSuite {
+}
