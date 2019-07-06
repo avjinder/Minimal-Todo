5ae2b05130a2563b1dac2f3007649509c8ce38d1
nirja75 <nirja75@gmail.com>
2019-07-06 06:31:17 -0400

    Test Case Passed

--- app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/Main/MainActivityTest_Nirja.java
+++ app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/Main/MainActivityTest_Nirja.java
@@ -1,3 +1,4 @@
+
 package com.example.avjindersinghsekhon.minimaltodo.Main;
 
 
@@ -6,6 +7,7 @@ import android.provider.Contacts;
 import android.provider.ContactsContract;
 
 import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import androidx.test.runner.AndroidJUnitRunner;
@@ -31,19 +33,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MainActivityTest_Nirja {
     @Rule
-    public ActivityTestRule mActivityRule = new ActivityTestRule(MainActivityTest_Nirja.class);
-
-
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.avjindersinghsekhon.minimaltodo.Main",
-                appContext.getPackageName());
-    }
-
-
+    public ActivityTestRule mActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Test
     public void activityLaunch() {
@@ -59,26 +49,42 @@ public class MainActivityTest_Nirja {
         onView(withId(R.id.userToDoRemindMeTextView)).check(matches(isDisplayed()));
         // Remind me text is visible or not
         onView(withId(R.id.copyclipboard)).check(matches(isDisplayed()));
-        // copy to clipboard
+        // matches whether copy to clipboard button is present or not
         onView(withId(R.id.makeToDoFloatingActionButton)).check(matches(isDisplayed()));
-        //arrow button
+        //matches whether arrow button is present or not
         onView(withId(R.id.toDoHasDateSwitchCompat)).check(matches(isDisplayed()));
-        //slider
+        //matches whether slider is present or not
+
     }
 
     @Test
-    public void textInputOutput() {
+    public void textInputOutput(){
+        onView(withId(R.id.addToDoItemFAB)).perform(click());
+        // click the add (+) button, to add a new todo list
         onView(withId(R.id.userToDoEditText)).perform(
                 typeText("This is a test."));
+        //enter the task to be performed
         onView(withId(R.id.userToDoDescription)).perform(
                 typeText("This is also a test."));
-
+        //describe the task to be performed
         onView(withId(R.id.makeToDoFloatingActionButton)).perform(click());
-        onView(withId(R.id.toDoListItemTextview)).check(
-                matches(withText("This is a test.")));
+        //click the floating button to save the task
+        onView(withId(R.id.toDoRecyclerView)).perform(RecyclerViewActions.scrollToPosition(6));
+        // Scroll to view item at a particular location
+        onView(withId(R.id.toDoRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(6,click()));
+        // click the item to view the task
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
 
 
