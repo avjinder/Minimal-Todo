ff7fc2893aa0cd5e4391d4d894eea68ec21de37f
TimKardol <tnmkardol@hotmail.com>
2020-10-30 20:22:56 +0100

    Fix bug in AddToDoActivity.java

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddToDo/AddToDoActivity.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddToDo/AddToDoActivity.java
@@ -12,7 +12,6 @@ public class AddToDoActivity extends AppDefaultActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        lets break this code
     }
 
     @Override
