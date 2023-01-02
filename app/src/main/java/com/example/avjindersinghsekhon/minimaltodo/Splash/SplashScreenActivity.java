1b23d7abe953c54d8405397f989f4dd79624685a
Ankit Sharma <ankit.sharma722327@gmail.com>
2023-01-02 15:46:16 +0530

    Add Splash Screen

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Splash/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.avjindersinghsekhon.minimaltodo.Splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.avjindersinghsekhon.minimaltodo.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
